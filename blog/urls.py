8f8b56abb56ce2317d4a3fc9560627ea72846121
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2022-01-07 00:42:05 +0000

    Added comments

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.show, name='show'),
+    path('<int:post_id>/share', views.post_share, name='post_share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
 
 def index(request):
     posts = Post.published.all()
-    return render(request, 'blog/posts/index.html', {'posts': posts})
+    paginator = Paginator(posts, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/posts/index.html', {'posts': posts, 'page': page})
+
+
+class IndexView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/posts/index.html'
 
 
 def show(request, year, month, day, post):
@@ -17,4 +36,32 @@ def show(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day,
                              )
-    return render(request, 'blog/posts/show.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        form = CommentForm()
+    return render(request, 'blog/posts/show.html', {'post': post, 'comments': comments, 'comment': new_comment, 'comment_form': form})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message,
+                      'insightnetwork.15@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/posts/share.html', {'post': post, 'form': form, 'sent': sent})
